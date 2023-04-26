ecd04d8a9a218b5b3da228f2c117436b678b43ba
unknown <mparizeau@pdftron.com>
2023-04-26 06:16:41 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+Core.setWorkerPath('./webviewer-lib/core');
+
+const mergeImage = async () => {
+  const initialDocument = await Core.createDocument('./images/file_example_JPG_1MB.jpg', { loadAsPDF: true });
+
+  for (let i = 0; i < 40; i++) {
+    await initialDocument.mergeDocument('./images/file_example_JPG_1MB.jpg');
+    console.log(`merged ${i} 1MB`);
+    await initialDocument.mergeDocument('./images/file_example_JPG_2500kB.jpg');
+    console.log(`merged ${i} 2.5MB`);
+  }
+  console.log('done!');
+
+  const fileData = await initialDocument.getFileData();
+  const data = new Uint8Array(fileData);
+  const blobData = new Blob([data], { type: 'application/pdf' });
+  saveAs(blobData, 'merged.pdf');
+};
+
+mergeImage();
